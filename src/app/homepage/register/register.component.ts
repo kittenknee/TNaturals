688a6e3660f43b5d202795d9da73bde8345fdb5e
kittenknee <kabirbagwe@gmail.com>
2023-12-31 22:20:05 +0530

    Register page 2

--- src/app/homepage/register/register.component.ts
+++ src/app/homepage/register/register.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { NavbarComponent } from '../navbar/navbar.component';
+
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [
+    CommonModule,
     ReactiveFormsModule,
     NavbarComponent,
     RouterOutlet,
@@ -17,24 +21,62 @@ import { NavbarComponent } from '../navbar/navbar.component';
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent implements OnInit {
+  registerForm!: FormGroup;
+  toastmessage!: string;
 
-  register = new FormGroup({
-    name: new FormControl(''),
-    contact: new FormControl(''),
-    type: new FormControl(''),
-    cust: new FormControl(''),
-    supp: new FormControl(''),
-    uname: new FormControl(''),
-    pword: new FormControl(''),
-    pword1: new FormControl('')
-  });
+  constructor(private fb: FormBuilder){}
 
   onSubmit(){
-
+    if(this.registerForm.valid){
+      console.table(this.registerForm.value);
+      // Add more functions here
+      this.toast("some data");
+      this.registerForm.reset();
+    }
+    else{
+      console.log('Register form filled is invalid')
+      this.registerForm.markAllAsTouched();
+    }
   }
 
-  ngOnInit(): void {
-      
+  get name(){
+    return this.registerForm.get('name');
+  }
+  get contact(){
+    return this.registerForm.get('contact');
+  }
+  get type(){
+    return this.registerForm.get('type');
+  }
+  get uname(){
+    return this.registerForm.get('uname');
+  }
+  get pword(){
+    return this.registerForm.get('pword');
+  }
+  get pword1(){
+    return this.registerForm.get('pword1');
+  }
+
+  ngOnInit(){
+    this.registerForm = this.fb.group({
+      name: new FormControl('',[Validators.required,Validators.minLength(5),Validators.maxLength(25),Validators.pattern("[a-zA-Z][a-zA-Z ]+")]),
+      contact: new FormControl('',[Validators.required,Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]),
+      type: new FormControl('',[Validators.required]),
+      uname: new FormControl('',[Validators.required,Validators.minLength(5),Validators.maxLength(20),Validators.pattern("[a-zA-Z][a-zA-Z ]+")]),
+      pword: new FormControl('',[Validators.required,Validators.minLength(5),Validators.maxLength(20),Validators.pattern("[a-zA-Z][a-zA-Z ]+")]),
+      pword1: new FormControl('',[Validators.required,Validators.minLength(5),Validators.maxLength(20),Validators.pattern("[a-zA-Z][a-zA-Z ]+")])
+    });    
+    this.toast("NgOnInit data");
+  }
+
+  toast(data: string){
+    this.toastmessage = data;
+    var toastElList = document.getElementById('registerResponseToast');
+    var toast = new bootstrap.Toast(toastElList, { delay: 3000 }); // Set your desired delay
+  
+    // Example: Show the toast
+    toast.show();
   }
 
 }
